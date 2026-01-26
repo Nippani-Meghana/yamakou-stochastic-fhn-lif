58bd674b02245a291e41f60bb198cf569813e5e5
Nippani-Meghana <meghananippani@gmail.com>
2026-01-26 12:35:39 +0530

    Added documentation

--- simulation/deterministic.py
+++ simulation/deterministic.py
@@ -4,10 +4,18 @@ import json
 
 from Models.FHN import FHN
 
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# __file__ is the path to the current Python script.
+# Path(__file__).resolve() converts it into an absolute path.
+# .parent goes one folder up. .parent.parent goes two folders up.
 BASE_DIR = Path(__file__).resolve().parent.parent  # points to project/
+
+# This uses Python’s pathlib “/” operator to join paths safely.
 json_path = BASE_DIR / "config" / "fhn_params.json"
 
+# Opens the JSON file for reading. The with statement ensures it automatically closes afterward.
 with open(json_path) as f:
+    # Reads the JSON file and converts it into a Python dictionary (dict).
     params = json.load(f)
 
 # Access parameters
