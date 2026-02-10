b10087bc397b99eeeb44c48bfaf3ee0d568cc56c
Nippani-Meghana <meghananippani@gmail.com>
2026-02-10 12:02:12 +0530

    Completed additive noise of FHN

--- simulation/additive_noise.py
+++ simulation/additive_noise.py
@@ -1,31 +1,11 @@
 import numpy as np
 from pathlib import Path
-import json
+import json 
 from Models.FHN import FHN
+from simulation import path_calling
 
 def additive_noise():
-
-    # BASE_DIR = Path(__file__).resolve().parent.parent
-    # __file__ is the path to the current Python script.
-    # Path(__file__).resolve() converts it into an absolute path.
-    # .parent goes one folder up. .parent.parent goes two folders up.
-    BASE_DIR = Path(__file__).resolve().parent.parent  # points to project/
-
-    # This uses Python’s pathlib “/” operator to join paths safely.
-    json_path = BASE_DIR / "config" / "fhn_params.json"
-
-    # Opens the JSON file for reading. The with statement ensures it automatically closes afterward.
-    with open(json_path) as f:
-        # Reads the JSON file and converts it into a Python dictionary (dict).
-        params = json.load(f)
-
-    
-    # Access parameters
-    I_ext = params["fhn_parameters"]["I_ext"]
-    a = params["fhn_parameters"]["a"]
-    b = params["fhn_parameters"]["b"]
-    tau = params["fhn_parameters"]["tau"]
-
+    I_ext,a,b,tau = path_calling()
     dt = 0.01        # timestep
     T = 1000           # total time
     steps = int(T/dt)
@@ -37,13 +17,15 @@ def additive_noise():
     neuron = FHN(a, b, tau, I_ext)
 
     # Initial conditions
-    v[0] = -1
-    w[0] = 0
+    v[0] = -1.00125
+    w[0] = -0.4
 
     # Time evolution loop
     for i in range(1, steps):
+        var = 0.02
+        noise = var * np.random.normal(0, 1) * np.sqrt(dt)
         v[i] = v[i-1] + neuron.f(v[i-1], w[i-1])*dt
-        w[i] = w[i-1] + neuron.g(v[i-1], w[i-1])*dt + 0.02
+        w[i] = w[i-1] + neuron.g(v[i-1], w[i-1]) * dt + noise
 
     print("v values:",v)
     print("w values:",w)
