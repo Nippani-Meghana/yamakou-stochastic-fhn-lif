9522983fe14265540eadaf9ef43f959dc44410cd
Nippani-Meghana <meghananippani@gmail.com>
2026-02-11 19:07:04 +0530

    Added the numerical method for multiplicative noise

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,3 +1,4 @@
 from .path_calling import path_calling
 from .deterministic import deterministic
 from .additive_noise import additive_noise
+from .multiplicative_noise import multiplicative_noise
--- simulation/multiplicative_noise.py
+++ simulation/multiplicative_noise.py
@@ -2,7 +2,22 @@ import numpy as np
 from Models.FHN import FHN
 from simulation import path_calling
 
-def additive_noise():
+def multiplicative_noise(v0,w0):
+    """
+    Simulates the FitzHugh-Nagumo (FHN) model with multiplicative stochastic noise 
+    using a Second-Order Stochastic Runge-Kutta (Heun) method.
+    
+    In this implementation, the noise intensity scales with the state variable 'w'.
+    The SRK method is used to maintain numerical stability and accuracy that 
+    Euler-Maruyama might lose when dealing with state-dependent noise.
+    
+    Args:
+        v0 (float): Initial condition for membrane potential.
+        w0 (float): Initial condition for recovery variable.
+        
+    Returns:
+        tuple: (v, w, v_e, w_e, J_e) arrays of states, equilibrium points, and Jacobian.
+    """
     I_ext,a,b,tau = path_calling()
     dt = 0.01        # timestep
     T = 1000           # total time
@@ -15,15 +30,17 @@ def additive_noise():
     neuron = FHN(a, b, tau, I_ext)
 
     # Initial conditions
-    v[0] = -1.00125
-    w[0] = -0.4
+    v[0] = v0
+    w[0] = w0
 
     # Time evolution loop
     for i in range(1, steps):
-        var = 0.02
-        noise = var * np.random.normal(0, 1) * np.sqrt(dt)
-        v[i] = v[i-1] + neuron.f(v[i-1], w[i-1])*dt
-        w[i] = w[i-1] + neuron.g(v[i-1], w[i-1]) * dt + noise
+        sigma = 0.02
+        delta_B = np.sqrt(dt) * np.random.normal(0, 1)
+        v_predictor = v[i-1] + neuron.f(v[i-1], w[i-1])*dt
+        w_predictor = w[i-1] + neuron.g(v[i-1], w[i-1]) * dt + (sigma*delta_B*w[i-1])
+        v[i] = v[i-1] + (1/2)*((neuron.f(v[i-1], w[i-1])) + (neuron.f(v_predictor, w_predictor)))*dt
+        w[i] = w[i-1] +(1/2)*((neuron.g(v[i-1], w[i-1]))+(neuron.g(v_predictor, w_predictor)))*dt + (1/2)*sigma*(w[i-1]+w_predictor)*delta_B
 
     print("v values:",v)
     print("w values:",w)
