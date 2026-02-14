419d4db5675fae181f5688555259f91ddfe8e77e
Nippani-Meghana <meghananippani@gmail.com>
2026-02-14 12:38:10 +0530

    Added timeseries in the dashboard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import simulation
-import visualization
+from visualization.timeseries import timeseries as plot_timeseries
 import Models
 import analysis
 import sys
@@ -12,15 +12,17 @@ while(True):
     print("4. Embedded LIF")
     print("5. Exit")
     ch = int(input('Please make your choice: '))
-    stats = analysis.ensemble_stats()
+    #stats = analysis.ensemble_stats()
 
     if(ch == 5):
         print("Program Terminated!")
         sys.exit()
 
-    count, timing,isi,cv,fano_factor = stats.trials_stats(ch)
+    #count, timing,isi,cv,fano_factor = stats.trials_stats(ch)
+    s = float(input("Please enter sigma value:"))
+    plot_timeseries(ch,s)
 
-    print("Trials and Spike Count: ",count)
-    print("ISI: ",isi)
-    print("CV:",cv)
-    print("Fano Factor: ",fano_factor)
+    #print("Trials and Spike Count: ",count)
+    #print("ISI: ",isi)
+    #print("CV:",cv)
+    #print("Fano Factor: ",fano_factor)
