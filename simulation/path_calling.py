f7ef4c9678d8a9d76a34011f28289f642613ec62
Nippani-Meghana <meghananippani@gmail.com>
2026-02-15 21:41:36 +0530

    Added path for LIF

--- simulation/path_calling.py
+++ simulation/path_calling.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import json
 
-def path_calling():
+def path_calling_fhn():
     # BASE_DIR = Path(__file__).resolve().parent.parent
     # __file__ is the path to the current Python script.
     # Path(__file__).resolve() converts it into an absolute path.
@@ -24,3 +24,28 @@ def path_calling():
     tau = params["fhn_parameters"]["tau"]
 
     return I_ext,a,b,tau
+
+def path_calling_lif():
+    # BASE_DIR = Path(__file__).resolve().parent.parent
+    # __file__ is the path to the current Python script.
+    # Path(__file__).resolve() converts it into an absolute path.
+    # .parent goes one folder up. .parent.parent goes two folders up.
+    BASE_DIR = Path(__file__).resolve().parent.parent  # points to project/
+
+    # This uses Python’s pathlib “/” operator to join paths safely.
+    json_path = BASE_DIR / "config" / "lif_params.json"
+
+    # Opens the JSON file for reading. The with statement ensures it automatically closes afterward.
+    with open(json_path) as f:
+    # Reads the JSON file and converts it into a Python dictionary (dict).
+        params = json.load(f)
+
+        
+    # Access parameters
+    I_ext = params["lif_parameters"]["I_ext"]
+    R = params["lif_parameters"]["R"]
+    V_r = params["lif_parameters"]["V_r"]
+    sigma = params["lif_parameters"]["sigma"]
+    tau = params["lif_parameters"]["tau"]
+    
+    return I_ext, R, V_r, sigma, tau
